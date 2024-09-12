32a8b2aace2460df0752c39f6ff47f5b9b715597
Akshat Dubey <akshatdubey0808@gmail.com>
2024-09-12 12:15:33 +0530

    created initial grid

---
+++ app/_constants/GallaryAssets.tsx
@@ -0,0 +1,95 @@
+
+type GalleryItem = {
+    id: number;
+    title: string;
+    videoURL: string;
+    imageURL: string;
+    imagePositioning: string;
+}
+
+export const GalleryItems: GalleryItem[] = [
+    {
+        id: 1,
+        title: "Bezaar",
+        videoURL: "https://res.cloudinary.com/djpbvhxfh/video/upload/v1726122136/srk-gallery/videos/bezaar_aj9yvx.mp4",
+        imageURL: "https://res.cloudinary.com/djpbvhxfh/image/upload/v1726121954/srk-gallery/images/2011-srk_ojxsp4.jpg",
+        imagePositioning: "object-top",
+    },
+    {
+        id: 2,
+        title: "Picture Abhi Baaki Hai",
+        videoURL: "https://res.cloudinary.com/djpbvhxfh/video/upload/v1726120254/srk-gallery/picture-abhi-baaki-hai_fkqgmb.mp4",
+        imageURL: "https://res.cloudinary.com/djpbvhxfh/image/upload/v1726121955/srk-gallery/images/beard_s1w6ka.jpg",
+        imagePositioning: "object-[center_20%]",
+    },
+    {
+        id: 3,
+        title: "Picture Abhi Baaki Hai",
+        videoURL: "https://res.cloudinary.com/djpbvhxfh/video/upload/v1726120254/srk-gallery/picture-abhi-baaki-hai_fkqgmb.mp4",
+        imageURL: "https://res.cloudinary.com/djpbvhxfh/image/upload/v1726121956/srk-gallery/images/guitar_o5pkms.jpg",
+        imagePositioning: "object-[center_90%]",
+    },
+    {
+        id: 4,
+        title: "Picture Abhi Baaki Hai",
+        videoURL: "https://res.cloudinary.com/djpbvhxfh/video/upload/v1726120254/srk-gallery/picture-abhi-baaki-hai_fkqgmb.mp4",
+        imageURL: "https://res.cloudinary.com/djpbvhxfh/image/upload/v1726121954/srk-gallery/images/shades_uwxcjx.jpg",
+        imagePositioning: "object-[center_70%]",
+    },
+    {
+        id: 5,
+        title: "Picture Abhi Baaki Hai",
+        videoURL: "https://res.cloudinary.com/djpbvhxfh/video/upload/v1726120254/srk-gallery/picture-abhi-baaki-hai_fkqgmb.mp4",
+        imageURL: "https://res.cloudinary.com/djpbvhxfh/image/upload/v1726121955/srk-gallery/images/blue-jacket_fqforl.jpg",
+        imagePositioning: "object-[center_35%]",
+    },
+    {
+        id: 6,
+        title: "Picture Abhi Baaki Hai",
+        videoURL: "https://res.cloudinary.com/djpbvhxfh/video/upload/v1726120254/srk-gallery/picture-abhi-baaki-hai_fkqgmb.mp4",
+        imageURL: "https://res.cloudinary.com/djpbvhxfh/image/upload/v1726121954/srk-gallery/images/suit_yekhqn.jpg",
+        imagePositioning: "object-top",
+    },
+    {
+        id: 7,
+        title: "Picture Abhi Baaki Hai",
+        videoURL: "https://res.cloudinary.com/djpbvhxfh/video/upload/v1726120254/srk-gallery/picture-abhi-baaki-hai_fkqgmb.mp4",
+        imageURL: "https://res.cloudinary.com/djpbvhxfh/image/upload/v1726121955/srk-gallery/images/smoking_uwpehw.jpg",
+        imagePositioning: "",
+    },
+    {
+        id: 8,
+        title: "Picture Abhi Baaki Hai",
+        videoURL: "https://res.cloudinary.com/djpbvhxfh/video/upload/v1726120254/srk-gallery/picture-abhi-baaki-hai_fkqgmb.mp4",
+        imageURL: "https://res.cloudinary.com/djpbvhxfh/image/upload/v1726121956/srk-gallery/images/mannat_nytku2.jpg",
+        imagePositioning: "",
+    },
+    {
+        id: 9,
+        title: "Picture Abhi Baaki Hai",
+        videoURL: "https://res.cloudinary.com/djpbvhxfh/video/upload/v1726120254/srk-gallery/picture-abhi-baaki-hai_fkqgmb.mp4",
+        imageURL: "https://res.cloudinary.com/djpbvhxfh/image/upload/v1726121956/srk-gallery/images/dunki_q2zhqm.jpg",
+        imagePositioning: "object-bottom",
+    },
+    {
+        id: 10,
+        title: "Picture Abhi Baaki Hai",
+        videoURL: "https://res.cloudinary.com/djpbvhxfh/video/upload/v1726120254/srk-gallery/picture-abhi-baaki-hai_fkqgmb.mp4",
+        imageURL: "https://res.cloudinary.com/djpbvhxfh/image/upload/v1726121955/srk-gallery/images/autograph_qa4w82.jpg",
+        imagePositioning: "object-top",
+    },
+    {
+        id: 11,
+        title: "Picture Abhi Baaki Hai",
+        videoURL: "https://res.cloudinary.com/djpbvhxfh/video/upload/v1726120254/srk-gallery/picture-abhi-baaki-hai_fkqgmb.mp4",
+        imageURL: "https://res.cloudinary.com/djpbvhxfh/image/upload/v1726121955/srk-gallery/images/dear-zindagi_qpoz8l.jpg",
+        imagePositioning: "",
+    },
+    {
+        id: 12,
+        title: "Picture Abhi Baaki Hai",
+        videoURL: "https://res.cloudinary.com/djpbvhxfh/video/upload/v1726120254/srk-gallery/picture-abhi-baaki-hai_fkqgmb.mp4",
+        imageURL: "https://res.cloudinary.com/djpbvhxfh/image/upload/v1726121956/srk-gallery/images/pathaan_roxecg.jpg",
+        imagePositioning: "object-[center_75%]",
+    }
+]
